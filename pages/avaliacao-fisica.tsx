5b7ad917665a6044810613f80b377593cbbfe898
Kibryant <124307212+Kibryant@users.noreply.github.com>
2023-08-06 11:33:41 -0300

    mudanças nas pages.

--- pages/avaliacao-fisica.tsx
+++ pages/avaliacao-fisica.tsx
@@ -1,7 +1,6 @@
 const AvaliacaoFisica = () => {
   return (
-    <>
-    </>
+    <></>
   )
 }
 
--- pages/consultoria-online.tsx
+++ pages/consultoria-online.tsx
@@ -1,7 +1,6 @@
 const ConsultoriaOnline = () => {
   return (
-    <>
-    </>
+    <></>
   )
 }
 
