ad0c1a67886ca4138fd1211685ec4b0a78f7b013
Kibryant <124307212+Kibryant@users.noreply.github.com>
2023-09-11 12:43:59 -0300

    Update index.tsx

--- src/components/MySwiper/index.tsx
+++ src/components/MySwiper/index.tsx
@@ -4,7 +4,7 @@ import "swiper/css"
 import "swiper/css/free-mode"
 import "swiper/css/pagination"
 
-import { FreeMode, Pagination } from "swiper/modules"
+import { FreeMode, Pagination } from "swipe"
 
 const MySwiper = () => {
   return (
