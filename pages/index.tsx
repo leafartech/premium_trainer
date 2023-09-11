807566b3a3f5e6aa3d2e0ae6ef0c4cdad3d0b6e1
leafartech <119262053+leafartech@users.noreply.github.com>
2023-09-11 09:48:04 -0300

    api removed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,257 +1,235 @@
-import Head from 'next/head'
-import clientPromise from '../lib/mongodb'
-import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
+import Box from "components/Box"
+import ButtonCTA from "components/ButtonCTA"
+import Card from "components/Card"
+import ComparisionBox from "components/ComparasionBox"
+import FAQ from "components/FAQ"
+import Footer from "components/Footer"
+import H2 from "components/H2"
+import Header from "components/Header"
+import MiniBox from "components/MiniBox"
+import Quote from "components/Quote"
+import SecondCard from "components/SecondCard"
+import Section from "components/Section"
+import ServicesBox from "components/ServicesBox"
+import {
+  AcademicCapIcon,
+  ClipboardDocumentListIcon,
+  TrophyIcon,
+  ClipboardDocumentCheckIcon,
+  PresentationChartLineIcon,
+  ArrowPathRoundedSquareIcon,
+  BugAntIcon,
+  MapIcon
+} from "@heroicons/react/24/outline"
+import MySwiper from "components/MySwiper"
 
-type ConnectionStatus = {
-  isConnected: boolean
-}
-
-export const getServerSideProps: GetServerSideProps<
-  ConnectionStatus
-> = async () => {
-  try {
-    await clientPromise
-    // `await clientPromise` will use the default database passed in the MONGODB_URI
-    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
-    //
-    // `const client = await clientPromise`
-    // `const db = client.db("myDatabase")`
-    //
-    // Then you can execute queries against your database like so:
-    // db.find({}) or any of the MongoDB Node Driver commands
-
-    return {
-      props: { isConnected: true },
-    }
-  } catch (e) {
-    console.error(e)
-    return {
-      props: { isConnected: false },
-    }
-  }
-}
-
-export default function Home({
-  isConnected,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+const Home = () => {
   return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js with MongoDB!</a>
-        </h1>
-
-        {isConnected ? (
-          <h2 className="subtitle">You are connected to MongoDB</h2>
-        ) : (
-          <h2 className="subtitle">
-            You are NOT connected to MongoDB. Check the <code>README.md</code>{' '}
-            for instructions.
+    <div className="overflow-hidden">
+      <Header
+        topTitle="treinador"
+        description="Seja muito bem-vindo(a) ao ponto de partida para sua nova vida. Sou especialista em estratégias individualizadas de treinamento, e estou aqui para elevar a sua qualidade de vida a novos patamares."
+        ctaText="Quero saber mais!"
+        ctaHref="#produtos"
+        imageHeaderPath="inicio"
+      >
+        <H2 header={true}>
+          Te ajudo a conquistar o{" "}
+          <span className="text-brandRed-500">shape dos seus sonhos</span>
+        </H2>
+      </Header>
+      <main className="sm:-mt-16">
+        <Section>
+          <div className="flex flex-col w-full">
+            <div className="w-full flex justify-center md:hidden">
+              <img
+                className="w-myLg mt-3 rounded-md"
+                src="./images/inicio0.png"
+                alt="Foto do João Navarro"
+              />
+            </div>
+            <div className="flex flex-col sm:items-center">
+              <div className="w-full mt-4 md:mt-0 px-1 md:flex md:justify-center">
+                <MiniBox text="treinador" />
+              </div>
+              <div className="my-4 md:text-center">
+                <H2 center={true}>Quem é João Navarro</H2>
+              </div>
+              <p className="text-zinc-300 text-justify max-w-4xl md:text-xl sm:text-center">
+                Apaixonado por saúde e bem estar. Bacharel em educação
+                física, pós graduado em Bodybuilding & Coach, pela Uniguaçu, e mais de 9 anos de experiência no mundo do treinamento,
+                ele é um profissional dedicado e compromissado com o sucesso de
+                seus alunos.{" "}
+              </p>
+            </div>
+            <div className="w-full hidden md:justify-center md:flex">
+              <img
+                className="w-myLg mt-3 max-w-2xl mb-4"
+                src="./images/inicio0.png"
+                alt="Foto do João Navarro"
+              />
+            </div>
+            <div className="py-3 gap-4 w-full flex flex-col justify-center items-start md:flex-row md:justify-between md:gap-10">
+              <Card
+                Icon={AcademicCapIcon}
+                title="Formação Acadêmica"
+                description="Bacharelado em educação física e pós graduado em Bodybuilding & Coach, pela Uniguaçu."
+              />
+              <Card
+                Icon={ClipboardDocumentListIcon}
+                title="Experiência na Área"
+                description="Há mais de 9 anos fazendo parte de grandes transformações na vida dos meus alunos."
+              />
+              <Card
+                Icon={MapIcon}
+                title="Mentalidade"
+                description="Exploro como superar obstáculos e desafios com meu aluno, aproveitando cada etapa como um degrau em direção ao progresso."
+              />
+            </div>
+            <div className="w-full md:hidden">
+              <ButtonCTA href="#produtos">Quero saber mais!</ButtonCTA>
+            </div>
+          </div>
+        </Section>
+        <div className="hidden md:block line-gradient h-1"></div>
+        <Section>
+          <div className="w-full md:hidden">
+            <img
+              className="w-myLg md:w-myXl mb-4"
+              src="./images/inicio2.png"
+              alt="Foto do João Navarro"
+            />
+          </div>
+          <div className="md:text-center">
+            <H2 center={true}>
+              O meu trabalho é ideal para{" "}
+              <span className="text-brandRed-500">você que:</span>
+            </H2>
+            <div className="sm:mt-16 sm:mb-24 flex flex-col md:flex-row items-start sm:justify-center sm:items-center sm:gap-12">
+              <div>
+                <SecondCard title="Quer tratamento de lesões." />
+                <SecondCard title="Busca produtividade no dia a dia." />
+                <SecondCard title="Quer uma reabilitação mais segura." />
+              </div>
+              <div>
+                <SecondCard title="Quer conquistar o shape do seus sonhos." />
+                <SecondCard title="Preza por saúde e qualidade de vida." />
+                <SecondCard title="Quer vencer a depressão e a ansiedade." />
+              </div>
+            </div>
+          </div>
+          <div className="w-full md:flex md:gap-5 md:justify-between md:mt-4 md:items-center mt-6">
+            <div className="md:flex md:flex-col md:items-start">
+              <H2>
+                Mude sua vida <span className="text-brandRed-500">hoje!</span>
+              </H2>
+              <Quote
+                firstWord="A"
+                Citation="prioridade do meu trabalho é cuidarmos da sua saúde de forma responsável! Através de uma abordagem individualizada, eu consigo proporcionar treinamentos de excelência, contribuindo para uma qualidade de vida e sensação de bem-estar excepcionais em sua vida!"
+              />
+              <div className="w-full sm:max-w-[256px] md:mt-2 mt-6">
+                <ButtonCTA href="#produtos">Quero saber mais!</ButtonCTA>
+              </div>
+            </div>
+            <div className="w-full sm:max-w-[540px]">
+              <img
+                className="w-myLg md:w-myXl mt-4"
+                src="./images/inicio3.png"
+                alt="Foto do João Navarro"
+              />
+            </div>
+          </div>
+        </Section>
+        <Section>
+          <H2 center={true}>
+            Confira alguns <span className="text-brandRed-500">feedbacks</span>
+          </H2>
+          <div className="px-4">
+            <MySwiper />
+          </div>
+        </Section>
+        <Section>
+          <div className="md:flex md:flex-col md:justify-center md:items-center sm:mb-12">
+            <div className="md:text-center">
+              <H2>
+                Por que meus alunos têm tantos{" "}
+                <span className="text-brandRed-500">resultados?</span>
+              </H2>
+            </div>
+            <div className="flex flex-col md:flex-row gap-4 md:gap-6 mt-6 sm:mt-12">
+              <div className="flex flex-col gap-4">
+                <Box
+                  Icon={TrophyIcon}
+                  boxTitle="Identificação dos objetivos"
+                  boxDescription="Compreendo que cada indivíduo tem suas próprias necessidades e ambições e é por isso que trabalho em estreita colaboração com cada aluno para definir metas claras e alcançáveis."
+                />
+                <Box
+                  Icon={ClipboardDocumentCheckIcon}
+                  boxTitle="Avaliações"
+                  boxDescription="Desde o início, eu dou muito valor a realizar avaliações detalhadas. Essas avaliações me permitem entender o estado físico mental de cada aluno e pontos para melhorar."
+                />
+              </div>
+              <div className="flex flex-col gap-4 md:mt-4">
+                <Box
+                  Icon={PresentationChartLineIcon}
+                  boxTitle="Periodização"
+                  boxDescription="Divido nossos programas de treinamento em diferentes fases, cada uma com seus próprios objetivos específicos. Essa abordagem permite uma progressão gradual e planejada."
+                />
+                <Box
+                  Icon={ArrowPathRoundedSquareIcon}
+                  boxTitle="Reavaliação"
+                  boxDescription="Isso garante que nossos programas de treinamento estejam sempre alinhados com as metas em andamento e que os resultados positivos sejam mantidos a longo prazo."
+                />
+              </div>
+            </div>
+            <div className="mt-9 w-full max-w-lg md:flex md:justify-center md:items-center">
+              <ButtonCTA href="#produtos">Quero saber mais</ButtonCTA>
+            </div>
+          </div>
+        </Section>
+        <Section>
+          <div className="mb-6 md:hidden">
+            <Quote
+              firstWord="Todos"
+              Citation="esses resultados acima são frutos de muito trabalho e dedicação de ambas as partes. Fora isso, o desenvolvimento de uma estratégia adaptada e específica para cada aluno foi um grande pilar no progresso geral."
+            />
+          </div>
+          <ComparisionBox />
+        </Section>
+        <Section>
+          <div className="md:text-center" id="produtos">
+            <H2 center={true}>
+              Conheça <span className="text-brandRed-500">meus serviços</span>
+            </H2>
+          </div>
+          <div className="flex flex-col sm:grid sm:grid-cols-3 gap-12 justify-center items-center mt-5">
+            <ServicesBox
+              href="/consultoria-presencial"
+              product="Consultoria Presencial"
+              bottomSentence="Sua melhor forma"
+            />
+            <ServicesBox
+              href="/consultoria-online"
+              product="Consultoria Online"
+              bottomSentence="MUDE, TREINE, SUPERE"
+            />
+            <ServicesBox
+              href="/avaliacao-fisica"
+              product="Avalição Física"
+              bottomSentence="MOLDANDO SEU FÍSICO"
+            />
+          </div>
+        </Section>
+        <Section>
+          <h2 className="tracking-[12px] text-center text-2xl sm:text-4xl text-brandWhite">
+            FAQ
           </h2>
-        )}
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+          <FAQ />
+        </Section>
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .subtitle {
-          font-size: 2rem;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+      <Footer />
     </div>
   )
 }
+
+export default Home
