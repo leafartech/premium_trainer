0590decdd31f997ef34fc6f1939b8227e817284c
Kibryant <arthurgustavon@gmail.com>
2024-09-21 14:40:15 -0300

    fix: navbar in mobile

--- src/components/Navbar/NavbarItems/index.tsx
+++ src/components/Navbar/NavbarItems/index.tsx
@@ -1,24 +1,50 @@
+'use client'
+
 import {
   ComputerDesktopIcon,
   HomeIcon,
   UsersIcon,
   IdentificationIcon,
 } from '@heroicons/react/24/outline'
-import { useNavbar } from '@/hooks/useNavbar'
 import NavbarLink from '../NavbarLink'
 import Image from 'next/image'
-import NavbarToggle from '../NavbarToggle'
+import { useState } from 'react'
 
 const NavbarItems = () => {
-  const { navbarToggle } = useNavbar()
+  const [navbarToggle, setNavbarToggle] = useState(false)
+
+  function toggleNavbar() {
+    setNavbarToggle((prevState) => !prevState)
+  }
 
   return (
     <>
-      <NavbarToggle />
+      <button
+        className="cursor-pointer md:hidden absolute right-3 h-4 w-6 flex justify-between items-center flex-col z-10"
+        onClick={toggleNavbar}
+      >
+        <div
+          className={`bg-brandWhite w-full rounded transition h-[1.5px] ${
+            navbarToggle &&
+            'absolute rotate-[-45deg] top-[50%] translate-y-[-50%]'
+          } `}
+        ></div>
+        <div
+          className={`bg-brandWhite w-full rounded transition h-[1.5px] ${
+            navbarToggle && 'opacity-0'
+          }`}
+        ></div>
+        <div
+          className={`bg-brandWhite w-full rounded transition h-[1.5px] ${
+            navbarToggle &&
+            'absolute rotate-[45deg] bottom-[50%] translate-y-[50%]'
+          }`}
+        ></div>
+      </button>
       <ul
         className={`sm:ms-24 transition duration-300 w-screen space-y-4 top-0 p-4 bg-black absolute min-h-screen md:min-h-full md:bg-transparent md:transform-none
          md:p-3 md:w-[600px] md:flex md:h-full md:relative md:space-y-0 md:justify-around md:right-4 md:items-center ${
-           !navbarToggle ? 'translate-x-full' : 'translate-x-0'
+           navbarToggle ? 'translate-x-0' : 'translate-x-full'
          } `}
       >
         <li>
